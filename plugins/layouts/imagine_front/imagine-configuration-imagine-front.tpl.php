39fac554a3441fc65cd87187f90d10f86b2e8d08
Kris <kris@meridian-ds.com>
2011-04-23 00:14:22 -0500

    adding the first layout plugin, this is very rough and is totally untested at this point

---
+++ plugins/layouts/imagine_front/imagine-configuration-imagine-front.tpl.php
@@ -0,0 +1,14 @@
+<div class="panel-display imagine-front clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="panel-panel imagine-top">
+    <div><?php print $content['top']; ?></div>
+  </div>
+  <div class="panel-panel imagine-top">
+    <div><?php print $content['left']; ?></div>
+  </div>
+  <div class="panel-panel imagine-top">
+    <div><?php print $content['right']; ?></div>
+  </div>
+  <div class="panel-panel imagine-bottom">
+    <div><?php print $content['bottom']; ?></div>
+  </div>
+</div>
